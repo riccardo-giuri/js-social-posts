ad2a6cb65286f4b7463753a3ca780f1752470c15
riccardo-giuri <richicryst@gmail.com>
2023-07-19 02:09:14 +0200

    generating a single card in HTML with JS

--- js/main.js
+++ js/main.js
@@ -56,14 +56,18 @@ const posts = [
     }
 ];
 
+const postContainer = document.querySelector("#container");
+
 const postList = [];
 
-createPost(posts);
+createPostObject(posts);
 
 console.log(postList);
 
+postContainer?.append(createPostElement(postList[0]));
 
-function createPost(dataArray) {
+
+function createPostObject(dataArray) {
     dataArray.forEach((element) => {
         const newPost = {};
 
@@ -93,4 +97,111 @@ function formattingAmericanTime(timeToConvert) {
     const americanData = `${timeArray[1]}-${timeArray[2]}-${timeArray[0]}`;
 
     return americanData;
+}
+
+function createPostElement(elementData) {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+
+    postElement.append(createPostHeader(elementData));
+    postElement.append(createPostText(elementData));
+    postElement.append(createPostImage(elementData));
+    postElement.append(createPostFooter(elementData));
+
+    return postElement;
+}
+
+function createPostHeader(elementData) {
+    const postHeader = document.createElement("div");
+    postHeader.classList.add("post__header");
+
+    const postMeta = document.createElement("div");
+    postMeta.classList.add("post-meta");
+    postHeader.append(postMeta);
+
+    const postMetaIcon = document.createElement("div");
+    postMetaIcon.classList.add("post-meta__icon");
+    postMeta.append(postMetaIcon);
+
+    const metaIcon_img = document.createElement("img");
+    metaIcon_img.classList.add("profile-pic");
+    metaIcon_img.src = elementData.authorImage;
+    postMetaIcon.append(metaIcon_img);
+
+    const postMetaData = document.createElement("div");
+    postMetaData.classList.add("post-meta__data");
+    postMeta.append(postMetaData);
+
+    const postMetaAuthor = document.createElement("div");
+    postMetaAuthor.classList.add("post-meta__author");
+    postMetaAuthor.textContent = elementData.authorName;
+    postMetaData.append(postMetaAuthor);
+
+    const postMetaTime = document.createElement("div");
+    postMetaTime.classList.add("post-meta__time");
+    postMetaTime.textContent = elementData.postData;
+    postMetaData.append(postMetaTime);
+
+    return postHeader;
+}
+
+function createPostText(elementData) {
+    const postText = document.createElement("div");
+    postText.classList.add("post__text");
+    postText.textContent = elementData.postContent;
+
+    return postText;
+}
+
+function createPostImage(elementData) {
+    const postImageDiv = document.createElement("div");
+    postImageDiv.classList.add("post__image");
+
+    const postImg = document.createElement("img");
+    postImg.src = elementData.postImage;
+    postImageDiv.append(postImg);
+
+    return postImageDiv;
+}
+
+function createPostFooter(elementData) {
+    const postFooterDiv = document.createElement("div");
+    postFooterDiv.classList.add("post__footer");
+
+    const postLikesDiv = document.createElement("div");
+    postLikesDiv.classList.add("likes", "js-likes");
+    postFooterDiv.append(postLikesDiv);
+
+    const likesButtonDiv = document.createElement("div");
+    likesButtonDiv.classList.add("likes__cta");
+    postLikesDiv.append(likesButtonDiv);
+
+    const buttonLink = document.createElement("a");
+    buttonLink.classList.add("like-button", "js-like-button");
+    buttonLink.href = "#";
+    likesButtonDiv.append(buttonLink);
+
+    const ButtonLinkIcon = document.createElement("i");
+    ButtonLinkIcon.classList.add("like-button__icon", "fas", "fa-thumbs-up");
+    buttonLink.append(ButtonLinkIcon);
+
+    const buttonLinkText = document.createElement("span");
+    buttonLinkText.classList.add("like-button__label");
+    buttonLinkText.textContent = " Mi Piace";
+    buttonLink.append(buttonLinkText);
+
+    const likesCounter = document.createElement("div");
+    likesCounter.classList.add("likes__counter");
+
+    const likes = document.createElement("b");
+    likes.classList.add("js-likes-counter");
+    likes.id = "like-counter-1";
+    likes.textContent = elementData.postLikes;
+
+    likesCounter.innerHTML = "piace a ";
+    likesCounter.append(likes);
+    likesCounter.innerHTML += " persone"
+    postLikesDiv.append(likesCounter)
+
+    return postFooterDiv;
 }
